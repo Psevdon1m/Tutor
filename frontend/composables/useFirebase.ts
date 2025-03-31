c11b3e202bffa7665c547ea4b0dde1f4b50080ec
Psevdon1m <Valentine.oleynik@gmail.com>
2025-03-31 12:50:42 +0300

    Update useFirebase.ts

--- frontend/composables/useFirebase.ts
+++ frontend/composables/useFirebase.ts
@@ -6,18 +6,17 @@ import {
   type MessagePayload,
 } from "firebase/messaging";
 
-const config = useRuntimeConfig();
-const firebaseConfig = {
-  apiKey: config.public.FIREBASE_API_KEY as string,
-  authDomain: config.public.FIREBASE_AUTH_DOMAIN as string,
-  projectId: config.public.FIREBASE_PROJECT_ID as string,
-  storageBucket: config.public.FIREBASE_STORAGE_BUCKET as string,
-  messagingSenderId: config.public.FIREBASE_MESSAGING_SENDER_ID as string,
-  appId: config.public.FIREBASE_APP_ID as string,
-  measurementId: config.public.FIREBASE_MEASUREMENT_ID as string,
-};
-
 export const useFirebase = () => {
+  const config = useRuntimeConfig();
+  const firebaseConfig = {
+    apiKey: config.public.FIREBASE_API_KEY as string,
+    authDomain: config.public.FIREBASE_AUTH_DOMAIN as string,
+    projectId: config.public.FIREBASE_PROJECT_ID as string,
+    storageBucket: config.public.FIREBASE_STORAGE_BUCKET as string,
+    messagingSenderId: config.public.FIREBASE_MESSAGING_SENDER_ID as string,
+    appId: config.public.FIREBASE_APP_ID as string,
+    measurementId: config.public.FIREBASE_MEASUREMENT_ID as string,
+  };
   const app = initializeApp(firebaseConfig);
   const messaging = getMessaging(app);
   const vapidKey = useRuntimeConfig().public.vapidKey as string;
