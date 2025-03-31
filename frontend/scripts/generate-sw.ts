074fb9bd05685698c04a0b18287d39e6cccb0f10
Psevdon1m <Valentine.oleynik@gmail.com>
2025-03-31 12:46:50 +0300

    Revert "Update generate-sw.ts"
    This reverts commit 92d2863c6ef9c144ac01708a712a3973a8db7629.

--- frontend/scripts/generate-sw.ts
+++ frontend/scripts/generate-sw.ts
@@ -23,7 +23,7 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
+messaging.onBackgroundMessage(function(payload) {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
